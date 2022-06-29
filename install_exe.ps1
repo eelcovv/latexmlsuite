33b8bbe46ede1baa8fd4c37c6539a0b584e8a5f1
Eelco van Vliet <eelcovv@gmail.com>
2022-06-29 13:35:57 +0200

    pad nu correct

--- install_exe.ps1
+++ install_exe.ps1
@@ -1,4 +1,4 @@
-$TOOLS_DIR = "\\\\cbsp.nl\\Productie\\secundair\\DecentraleTools\\Output\\CBS_Python\Python3.9"
+$TOOLS_DIR = "\\cbsp.nl\Productie\secundair\DecentraleTools\Output\CBS_Python\Python3.9"
 $cmd="pip install . --no-build-isolation --prefix $TOOLS_DIR -U"
 $cmd = $cmd -replace '\s+', ' '
 Write-Output $cmd
